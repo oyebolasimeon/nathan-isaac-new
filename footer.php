96501ac60f9520426fedd09f7498cb7bd2a9eb6d
simeon oyebola <simeonoyekunleoyebola@gmail.com>
2023-01-07 22:54:16 +0100

    update

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
                 <div class="col-lg-4 col-md-6 footer-about">
                     <div class="d-flex flex-column align-items-center justify-content-center text-center h-100 bg-primary p-4">
                         <a href="index.html" class="navbar-brand">
-                            <h1 class="m-0 text-white d-flex"><img src="./img/logo.png" style="width: 50px; margin-right:10px; height: 50px;" alt="" srcset="">Nathan Isaac<br> Newsletter</h1>
+                            <h1 class="m-0 text-white d-flex"><img src="./img/logo.png" style="width: 50px; margin-right:10px; height: 50px;" alt="" srcset=""> Newsletter</h1>
                         </a>
                         <p class="mt-3 mb-4">
                             Kindly enter your valid and email so as to be getting my weekly Newsletter
