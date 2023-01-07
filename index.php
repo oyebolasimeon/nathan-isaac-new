292921962213a761d1919ce3610432956a08e36b
simeon oyebola <simeonoyekunleoyebola@gmail.com>
2023-01-07 22:49:51 +0100

    update

--- index.php
+++ index.php
@@ -396,7 +396,7 @@ Author, Intense Business Analyst, Developer, Strategist, Coach, Conference Speak
                 <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid" style="width: 100%; height: 50vh;" src="assets/book1.jpg" alt="">
+                            <img class="img-fluid" style="width: 100%; height: 50vh;" src="books/pocket-without-holes.jpg" alt="">
                             <!-- <div class="team-social">
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
@@ -413,7 +413,7 @@ Author, Intense Business Analyst, Developer, Strategist, Coach, Conference Speak
                 <div class="col-lg-4 wow slideInUp" data-wow-delay="0.6s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid" style="width: 100%; height: 50vh;" src="assets/book1.jpg" alt="">
+                            <img class="img-fluid" style="width: 100%; height: 50vh;" src="books/pocket-without-holes.jpg" alt="">
                             <!-- <div class="team-social">
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
@@ -431,7 +431,7 @@ Author, Intense Business Analyst, Developer, Strategist, Coach, Conference Speak
                 <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid" style="width: 100%; height: 50vh;" src="assets/book1.jpg" alt="">
+                            <img class="img-fluid" style="width: 100%; height: 50vh;" src="books/pocket-without-holes.jpg" alt="">
                             <!-- <div class="team-social">
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
                                 <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
