ebfed2f0e23ff91a577a9c3ac10391f6318ea98d
simeon oyebola <simeonoyekunleoyebola@gmail.com>
2023-01-07 22:50:54 +0100

    update

--- index.php
+++ index.php
@@ -390,7 +390,6 @@ Author, Intense Business Analyst, Developer, Strategist, Coach, Conference Speak
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
                 <h5 class="fw-bold text-primary text-uppercase">Resources</h5>
-                <h1 class="mb-0">See What We Have Made Available To You</h1>
             </div>
             <div class="row g-5">
                 <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
