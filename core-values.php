4f7d5e0314660c67f1b7bfcbbb3e5a809c899825
simeon oyebola <simeonoyekunleoyebola@gmail.com>
2023-01-07 23:32:50 +0100

    update

--- core-values.php
+++ core-values.php
@@ -4,10 +4,10 @@
         <div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 90px;">
             <div class="row py-5">
                 <div class="col-12 pt-lg-5 mt-lg-5 text-center">
-                    <h1 class="display-4 text-white animated zoomIn">Profile</h1>
+                    <h1 class="display-4 text-white animated zoomIn">Core Values</h1>
                     <a href="" class="h5 text-white">Home</a>
                     <i class="far fa-circle text-white px-2"></i>
-                    <a href="" class="h5 text-white">Profile</a>
+                    <a href="" class="h5 text-white">Core Values</a>
                 </div>
             </div>
         </div>
@@ -16,7 +16,62 @@
 
 
 
-
+    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
+                <h5 class="fw-bold text-primary text-uppercase">Core Values</h5>
+                <h1 class="mb-0">My core values are wrapped around my desire to HELP; to create visible impact in these various capacities</h1>
+            </div>
+            <div class="row g-5">
+                <div class="col-lg-4">
+                    <div class="row g-5">
+                        <div class="col-12 wow zoomIn" data-wow-delay="0.2s">
+                            <div class="bg-primary rounded d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 60px;">
+                                <i class="fa fa-cubes text-white"></i>
+                            </div>
+                            <h4>Humanity</h4>
+                            <p class="mb-0">I believe that compassion, mercy, empathy, kindness, patience and love altogether improve the quality of the life we live, the work we do and the services we render.</p>
+                        </div>
+                        <div class="col-12 wow zoomIn" data-wow-delay="0.6s">
+                            <div class="bg-primary rounded d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 60px;">
+                                <i class="fa fa-award text-white"></i>
+                            </div>
+                            <h4>Excellence</h4>
+                            <p class="mb-0">I believe in making conscious adjustments to achieve consistent peak performance. We never or will not always have the perfect conditions but we must make a commitment to make necessary adjustments, making the very best of every opportunity and resources for optimum results. I believe that excellence boils down to optimal utilization of available resources, and that this is how to live the best version of our lives and get the most admirable jobs done. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4  wow zoomIn" data-wow-delay="0.9s" style="min-height: 350px;">
+                    <div class="position-relative h-100">
+                        <img class="position-relative w-100 h-100 rounded wow zoomIn" style="border-radius: 30px 30px !important;" data-wow-delay="0.1s" src="assets/emblem.png" style="object-fit: cover;">
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="row g-5">
+                        <div class="col-12 wow zoomIn" data-wow-delay="0.4s">
+                            <div class="bg-primary rounded d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 60px;">
+                                <i class="fa fa-users-cog text-white"></i>
+                            </div>
+                            <h4>Leadership</h4>
+                            <p class="mb-0">I believe that one way to be significantly relevant to your world is live a life, build a business, a system or lead a change that can at least inspire someone.</p>
+                        </div>
+                        <div class="col-12 wow zoomIn" data-wow-delay="0.8s">
+                            <div class="bg-primary rounded d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 60px;">
+                                <i class="fa fa-phone-alt text-white"></i>
+                            </div>
+                            <h4>Passion</h4>
+                            <p class="mb-0">I believe that a passionless life is one that is only breathing.
+<br>A passionless work is almost a complete waste of our time and other resources
+<br>A passionless business or endeavor will never make a difference
+<br>A passionless speech is a sedating pill
+<br>A passionless leadership is an unfortunate situation<br>
+Passion is the power supply that makes the difference in anything we do and I believe that passion is a non negotiable requirement for outstanding results.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
