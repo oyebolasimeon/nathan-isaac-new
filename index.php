38a7dc435723377620117db1dfb1a3cf7fbbe528
simeon oyebola <simeonoyekunleoyebola@gmail.com>
2023-01-07 22:45:33 +0100

    update

--- index.php
+++ index.php
@@ -536,20 +536,7 @@ Author, Intense Business Analyst, Developer, Strategist, Coach, Conference Speak
 
 
     <!-- Vendor Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container py-5 mb-5">
-            <div class="bg-white">
-                <div class="owl-carousel vendor-carousel">
-                    <img src="partners/pwan.png" alt="">
-                    <img src="partners/access.png" alt="">
-                    <img src="partners/hotr.jpg" alt="">
-                    <img src="partners/wac.jpg" alt="">
-                    <img src="partners/gemstone.jpg" alt="">
-                    <img src="partners/abia.jpeg" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php require "partners-carousel.php" ?>
     <!-- Vendor End -->
     
 
---
+++ partners-carousel.php
@@ -0,0 +1,17 @@
+<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container py-5 mb-5">
+            <div class="bg-white">
+                <div class="owl-carousel vendor-carousel">
+                    <img src="partners/1.jpg" alt="">
+                    <img src="partners/2.jpg" alt="">
+                    <img src="partners/3.png" alt="">
+                    <img src="partners/4.png" alt="">
+                    <img src="partners/5.svg" alt="">
+                    <img src="partners/6.png" alt="">
+                    <img src="partners/7.png" alt="" srcset="">
+                    <img src="partners/8.jpg" alt="" srcset="">
+                    <img src="partners/9.jpg" alt="" srcset="">
+                </div>
+            </div>
+        </div>
+    </div>
