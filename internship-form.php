87cfa08cfd3189b5886041c3e2330e579730caaf
simeon oyebola <simeonoyekunleoyebola@gmail.com>
2023-01-16 19:29:21 +0100

    internship page deployed

---
+++ internship-form.php
@@ -0,0 +1,71 @@
+<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-7">
+                    <div class="section-title position-relative pb-3 mb-5">
+                        <h5 class="fw-bold text-primary text-uppercase">Open Application</h5>
+                        <h1 class="mb-0">INTERNSHIP APPLICATION 2023</h1>
+                    </div>
+                    <!-- <div class="row gx-3">
+                        <div class="col-sm-6 wow zoomIn" data-wow-delay="0.2s">
+                            <h5 class="mb-4"><i class="fa fa-reply text-primary me-3"></i>Reply within 24 hours</h5>
+                        </div>
+                        <div class="col-sm-6 wow zoomIn" data-wow-delay="0.4s">
+                            <h5 class="mb-4"><i class="fa fa-phone-alt text-primary me-3"></i>24 hrs telephone support</h5>
+                        </div>
+                    </div> -->
+                    <p class="mb-4">
+                    Internship application for the year 2023 is now open!
+
+The need to begin to pass on the torch early enough is now very glaring to me.
+
+The need to consciously train and extend a helping hand of experience, skill, wisdom, professionalism, Intentional Growth and Strategy to a younger generation of tech enthusiasts and creatives is eminent.
+
+Apply if you are interested and within the 18 -25 age bracket. Send your application and C.V to info@nathanisaac.org
+
+Application closes on the 31st of January 2023.
+                    </p>
+                    <div class="d-flex align-items-center mt-2 wow zoomIn" data-wow-delay="0.6s">
+                        <!-- <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
+                            <i class="fa fa-phone-alt text-white"></i>
+                        </div> -->
+                        <!-- <div class="ps-4">
+                            <h5 class="mb-2">Call to ask any question</h5>
+                            <h4 class="text-primary mb-0">+012 345 6789</h4>
+                        </div> -->
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="bg-primary rounded h-100 d-flex align-items-center p-5 wow zoomIn" data-wow-delay="0.9s">
+                        <form>
+                            <div class="row g-3">
+                                <div class="col-xl-12">
+                                    <input type="text" class="form-control bg-light border-0" placeholder="Your Name" style="height: 55px;">
+                                </div>
+                                <div class="col-12">
+                                    <input type="email" class="form-control bg-light border-0" placeholder="Your Email" style="height: 55px;">
+                                </div>
+                                <div class="col-12">
+                                    <input type="file" class="form-control bg-light border-0" placeholder="Upload Cv" style="height: 55px;">
+                                </div>
+                                <!-- <div class="col-12">
+                                    <select class="form-select bg-light border-0" style="height: 55px;">
+                                        <option selected>Select A Service</option>
+                                        <option value="1">Service 1</option>
+                                        <option value="2">Service 2</option>
+                                        <option value="3">Service 3</option>
+                                    </select>
+                                </div> -->
+                                <!-- <div class="col-12">
+                                    <textarea class="form-control bg-light border-0" rows="3" placeholder="Message"></textarea>
+                                </div> -->
+                                <div class="col-12">
+                                    <button class="btn btn-dark w-100 py-3" type="submit">Process Application</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
