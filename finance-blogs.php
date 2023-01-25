934059875659bffb71682535b6a31d587a983327
simeon oyebola <simeonoyekunleoyebola@gmail.com>
2023-01-25 21:05:59 +0100

    blog push

--- finance-blogs.php
+++ finance-blogs.php
@@ -62,16 +62,16 @@
                 <div class="col-lg-3 wow slideInUp" data-wow-delay="0.9s">
                     <div class="blog-item bg-light rounded overflow-hidden">
                         <div class="blog-img position-relative overflow-hidden">
-                            <img class="img-fluid" src="img/blog-3.jpg" alt="">
+                            <img class="img-fluid" src="blog/finance/wealth.jpeg" alt="">
                             <!-- <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="faith-blogs.html">Faith Blogs</a> -->
                         </div>
                         <div class="p-4">
                             <div class="d-flex mb-3">
-                                <small class="me-3"><i class="far fa-user text-primary me-2"></i>John Doe</small>
-                                <small><i class="far fa-calendar-alt text-primary me-2"></i>01 Jan, 2045</small>
+                                <small class="me-3"><i class="far fa-user text-primary me-2"></i>Nathan Isaac</small>
+                                <small><i class="far fa-calendar-alt text-primary me-2"></i>23 Jan, 2023</small>
                             </div>
-                            <h4 class="mb-3">How to build a website</h4>
-                            <p>Dolor et eos labore stet justo sed est sed sed sed dolor stet amet</p>
+                            <h4 class="mb-3">Wealth and Riches</h4>
+                            <p>Do not be lured into displaying riches at the expense of being wealthy.</p>
                             <a class="text-uppercase" href="faith-blogs.html">Read More <i class="bi bi-arrow-right"></i></a>
                         </div>
                     </div>
--- self-care-blogs.php
+++ self-care-blogs.php
@@ -27,7 +27,7 @@
                     <div class="blog-item bg-light rounded overflow-hidden">
                         <div class="blog-img position-relative overflow-hidden">
                             <img class="img-fluid" src="blog/self/mybody.jpeg" alt="">
-                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="business-blogs.html">My Beach Body</a>
+                            <!-- <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="business-blogs.html">My Beach Body</a> -->
                         </div>
                         <div class="p-4">
                             <div class="d-flex mb-3">
@@ -40,24 +40,24 @@
                         </div>
                     </div>
                 </div>
-                <!-- <div class="col-lg-3 wow slideInUp" data-wow-delay="0.6s">
+                 <div class="col-lg-3 wow slideInUp" data-wow-delay="0.6s">
                     <div class="blog-item bg-light rounded overflow-hidden">
                         <div class="blog-img position-relative overflow-hidden">
-                            <img class="img-fluid" src="img/blog-2.jpg" alt="">
-                            <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="finance-blogs.html">Finance Blog</a>
+                            <img class="img-fluid" src="blog/self/peace.jpeg" alt="">
+                            <!-- <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="finance-blogs.html">Finance Blog</a> -->
                         </div>
                         <div class="p-4">
                             <div class="d-flex mb-3">
-                                <small class="me-3"><i class="far fa-user text-primary me-2"></i>John Doe</small>
-                                <small><i class="far fa-calendar-alt text-primary me-2"></i>01 Jan, 2045</small>
+                                <small class="me-3"><i class="far fa-user text-primary me-2"></i>Nathan Isaac</small>
+                                <small><i class="far fa-calendar-alt text-primary me-2"></i>23 Jan, 2023</small>
                             </div>
-                            <h4 class="mb-3">How to build a website</h4>
-                            <p>Dolor et eos labore stet justo sed est sed sed sed dolor stet amet</p>
+                            <h4 class="mb-3">Peace & Prosperity</h4>
+                            <p>I'm all set and I'm about to go into a dimension of shaking very serious ...</p>
                             <a class="text-uppercase" href="finance-blogs.html">Read More <i class="bi bi-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 wow slideInUp" data-wow-delay="0.9s">
+               <!-- <div class="col-lg-3 wow slideInUp" data-wow-delay="0.9s">
                     <div class="blog-item bg-light rounded overflow-hidden">
                         <div class="blog-img position-relative overflow-hidden">
                             <img class="img-fluid" src="img/blog-3.jpg" alt="">
