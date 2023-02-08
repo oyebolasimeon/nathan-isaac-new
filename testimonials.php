92e6716792c20f6792f262365ddbb5cbe930cd12
simeon oyebola <simeonoyekunleoyebola@gmail.com>
2023-02-08 07:46:20 +0100

    new testimonials

--- testimonials.php
+++ testimonials.php
@@ -5,6 +5,167 @@
                 <h1 class="mb-0">What Our Clients Say.</h1>
             </div>
             <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.6s">
+
+            <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <img class="img-fluid rounded" src="testimonials/laiza.jpeg" style="width: 60px; height: 60px;" >
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Laiza King</h4>
+                            <small class="text-uppercase">Convener, MANIFEST Media</small><br>
+                            <small class="text-capitalize"><i style="color: #FFC448;" class="fa fa-map-marker"></i> Manhattan, New York</small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    Your trainings at the MANIFEST workshops were truly transformational. The feedback we received from the audience were positive as over 90% of them reporting how they gained clarity and the motivation to execute their goals/projects.
+
+Nathan has mastered the art of helping people go from idea to solution. He does this with charisma, backed up with the right frameworks and tools to help you thrive.
+                    </div>
+                </div>
+
+
+            <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <img class="img-fluid rounded" src="testimonials/iman.jpeg" style="width: 60px; height: 60px;" >
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Iman Noùjoum</h4>
+                            <small class="text-uppercase">Media & Fashion</small><br>
+                            <small class="text-capitalize"><i style="color: #FFC448;" class="fa fa-map-marker"></i>Nador, Morocco </small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    Thank you so much. Your session at the Mediterranean Alternatives forum held across Morocco, USA and Spain was huge. You brought so much insight during your session.
+
+Thank you also for your great contribution to my project on Social Media Advertising.
+                    </div>
+                </div>
+
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <img class="img-fluid rounded" src="testimonials/ridouan.jpeg" style="width: 60px; height: 60px;" >
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Ridouan Lamssarraf</h4>
+                            <small class="text-uppercase">President, Mediterranean Alternatives Forum</small><br>
+                            <small class="text-capitalize"><i style="color: #FFC448;" class="fa fa-map-marker"></i> Grand, Casablanca</small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    Being our guest at the Mediterranean Alternatives Forum brought us great delight. Thank you for giving us so much insight. All the participants were enamored and very grateful for the way you impacted us. We will love to have you again.
+
+Thank you
+                    </div>
+                </div>
+
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <img class="img-fluid rounded" src="testimonials/chioma.jpeg" style="width: 60px; height: 60px;" >
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Chioma Nnanna</h4>
+                            <small class="text-uppercase">Founder, Creatives In Business Africa, Public speaking Coach</small><br>
+                            <small class="text-capitalize"><i style="color: #FFC448;" class="fa fa-map-marker"></i> Portharcourt, Nigeria</small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    Your session at THE MILLION NAIRA IDEA webinar was so insightful and practical. You broke down the different stages of running a business in very simple words, carrying everyone along. As a brand, some business mistakes will definitely be avoided all thanks to you. Your choice of words, simplicity of delivery and connectivity is a very rare expertise.
+                    </div>
+                </div>
+
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <img class="img-fluid rounded" src="testimonials/jacob.jpeg" style="width: 60px; height: 60px;" >
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Emmanuel Jacob</h4>
+                            <small class="text-uppercase">International Speaker & Consultant</small><br>
+                            <small class="text-capitalize"><i style="color: #FFC448;" class="fa fa-map-marker"></i> Switzerland</small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    From your trainings and coaching I have I gained valuable insights on team building and leadership. I am amazed at How you simplify complex concepts with the key focus of sustainable solutions.
+                    </div>
+                </div>
+
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <img class="img-fluid rounded" src="testimonials/jeff.jpeg" style="width: 60px; height: 60px;" >
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Jeff Anozie</h4>
+                            <small class="text-uppercase">Marketing</small><br>
+                            <small class="text-capitalize"><i style="color: #FFC448;" class="fa fa-map-marker"></i> Enugu, Nigeria</small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    I have gained a great level of direction at certain times of confusion from you.  I love the calmness and patience with which you communicate so that everyone is carried along.
+                    </div>
+                </div>
+
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <img class="img-fluid rounded" src="testimonials/ethel.jpeg" style="width: 60px; height: 60px;" >
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Ethel Okonkwo</h4>
+                            <small class="text-uppercase">Entrepreneu</small><br>
+                            <small class="text-capitalize"><i style="color: #FFC448;" class="fa fa-map-marker"></i> Lagos, Nigeria</small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    Your training has been of tremendous help to me in decision making. The wisdom and experience with which you deliver is unmatched.
+                    </div>
+                </div>
+
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <img class="img-fluid rounded" src="testimonials/sydney.jpeg" style="width: 60px; height: 60px;" >
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Sydney Reo</h4>
+                            <small class="text-uppercase">Real Estate Entrepreneur</small><br>
+                            <small class="text-capitalize"><i style="color: #FFC448;" class="fa fa-map-marker"></i> Warri, Nigeria</small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    I got a grip of Strategic planning and presentation in public speaking from your coaching.
+
+I love your ability to listen and understand each business challenge and proffering the perfect business solutions and opportunities almost instantaneously
+                    </div>
+                </div>
+
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <img class="img-fluid rounded" src="testimonials/edith.jpeg" style="width: 60px; height: 60px;" >
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Edith Aigbefo</h4>
+                            <small class="text-uppercase">Real Estate Consultant</small><br>
+                            <small class="text-capitalize"><i style="color: #FFC448;" class="fa fa-map-marker"></i> Lagos, Nigeria</small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    Your training has impacted me positively and made me grow; it really broadened my knowledge. You have a unique way of coaching that is second to none and I really admire that aspect.
+                    </div>
+                </div>
+
+                <div class="testimonial-item bg-light my-4">
+                    <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                        <img class="img-fluid rounded" src="testimonials/salome.jpeg" style="width: 60px; height: 60px;" >
+                        <div class="ps-4">
+                            <h4 class="text-primary mb-1">Salome Omaka</h4>
+                            <small class="text-uppercase">Business Owner</small><br>
+                            <small class="text-capitalize"><i style="color: #FFC448;" class="fa fa-map-marker"></i> Lagos, Nigeria</small>
+                        </div>
+                    </div>
+                    <div class="pt-4 pb-5 px-5">
+                    It's a privilege to tell the world how your training has impacted me. I can say they are transformational, igniting, and innovative.
+Especially your training on leadership, team building, sure to goal marketing skills were result proofed.
+
+And to anyone who wants it better, Nathan Isaac is the one.
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+            <!-- new -->
                 <div class="testimonial-item bg-light my-4">
                     <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
                         <img class="img-fluid rounded" src="testimonials/jacob.jpeg" style="width: 60px; height: 60px;" >
